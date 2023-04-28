749503a67e155ef7ef83c950c48393403bc74620
CCotuna <calin.cotuna@emanuel.ro>
2023-04-28 08:49:59 +0300

    updated

--- vars.js
+++ vars.js
@@ -1,19 +1,28 @@
+let smileFaceCalin = {
+    x: 180,
+    y: 50,
+    speed:3,
+    ray:20,
+    sensX:1,
+    sensY:1
+}
+
 let moveS1X = 180;
 let moveS1Y = 50;
 let speedS1 =5;
 rayS1 = 20;
 
-let moveS2X = 180;
-let moveS2Y = 50;
-let speedS2 = 3;
-rayS2 = 20;
+// let moveS2X = 180;
+// let moveS2Y = 50;
+// let speedS2 = 3;
+// rayS2 = 20;
 
 
 let sensS1X = 1;
 let sensS1Y = 1;
 
-let sensS2X = 1;
-let sensS2Y = 1;
+// let sensS2X = 1;
+// let sensS2Y = 1;
 
 let limitTop = 0;
 let limitBottom = 600;
